f1626c26eab29fc9c652e70a8a8bc4a69584cb4f
Eduardo Correia <skelozard1@gmail.com>
2021-09-19 14:36:32 +0100

    Skeleton page done with Tailwind CSS and EJS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+        fontSize: {
+            sm: "1.2rem",
+            base: "2rem",
+            lg: "2.4rem"
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
